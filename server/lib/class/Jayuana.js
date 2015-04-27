6e924bdd4e09d58ee830a564ab494b8a2a1d0621
Yoh <yohann.m@gmail.com>
2015-04-27 18:48:07 -0500

    move all the lib folder in the server folder

---
+++ server/lib/class/Jayuana.js
@@ -0,0 +1,88 @@
+
+Jayuana = (function(){
+  "use strict";
+  var J = function () {
+    if (this instanceof J){
+
+    }
+    else if (J.db === undefined){
+      J.db = new Mongo.Collection("jayuanaDb");
+    }
+    else{
+      throw J.error("Jayuana", "called twice");
+    }
+  };
+
+  // METHODS:
+  J.prototype = {};
+
+  // STATICS METHODS:
+  J.add = function(obj, type, name, start, callback){
+    var objUnderTest, element, id, data, fileName;
+    var fs = Npm.require('fs');
+
+    name = name || '';
+    start = start || false;
+    element = {
+      name: name,
+      type: type,
+      start: start,
+      available: false,
+      path: 'unknown'
+    };
+
+    if((type !== "EJSON") && (type !== "code") && (type !== "file")){
+      throw J.error("Jayuana.add", "type not defined correctly");
+    }
+
+    switch (type){
+      case "EJSON":
+        objUnderTest = obj;
+        data = EJSON.stringify(obj);
+        break;
+
+      case "code":
+        try{
+          eval('objUnderTest = ' + obj); //jshint ignore:line
+        }
+        catch (e){
+          throw J.error("Jayuana.add", "eval(): " + e.message);
+        }
+
+        break;
+
+      case "file":
+
+        break;
+    }
+
+    if (objUnderTest === undefined){
+      throw J.error("Jayuana.add", "undefined object");
+    }
+    objUnderTest = undefined;
+
+    id = J.db.insert(element);
+    fileName = C.FILES_FOLDER + id;
+
+    if (!fs){
+      throw J.error("fs missing");
+    }
+
+    fs.writeFile(fileName, data, function (e) {
+      if (e) {
+        J.db.remove(id);
+        throw J.error("Jayuana.add", "writeFile: " + e.message);
+      }
+      else{
+        J.db.update({_id: id},{$set: {
+          available: true,
+          path: fileName}});
+
+        callback(id);
+      }
+    });
+  };
+
+  return J;
+})();
+
---
+++ server/start/constant.js
@@ -0,0 +1,8 @@
+/**
+ * Created by yoh on 4/27/15.
+ */
+
+
+C = {  // jshint ignore:line
+  PATH_FILES: "./private/packagesFiles" // jshint ignore:line
+};
