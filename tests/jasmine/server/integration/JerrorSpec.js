51023542dc0726360452506a35aa2a709995e8c4
Yoh <yohann.m@gmail.com>
2015-12-25 03:13:44 +0800

    moved all Jerror tests to server-integration

--- tests/jasmine/server/integration/JerrorSpec.js
+++ tests/jasmine/server/integration/JerrorSpec.js
@@ -1,10 +1,37 @@
-xdescribe("J.Error", function () {
+describe("J.Error", function () {
 
-  it("should throw an Error", function () {
+  var errorMsg = "error";
+  var reason = "reason";
+  var details = "details";
+
+  it("should return 'unknown Error' and stack if call " +
+    "without arguments",
+    function () {
+      var testError = new J.Error();
+      expect(testError.error).toEqual("unknown Error");
+      expect(testError.reason).toEqual(" ");
+      expect(testError.details).toBeDefined();
+    });
+  it("should return the Error and reason messages and stack if" +
+    "called with two arguments", function () {
+    var testError = new J.Error(errorMsg, reason);
+    expect(testError.error).toEqual(errorMsg);
+    expect(testError.reason).toEqual(reason);
+    expect(testError.details).toBeDefined();
+  });
+  it("should return the Error, reason and details messages if" +
+    "called with three arguments", function () {
+    var testError = new J.Error(errorMsg, reason, details);
+    expect(testError.error).toEqual(errorMsg);
+    expect(testError.reason).toEqual(reason);
+    expect(testError.details).toEqual(details);
+  });
+
+  xit("should throw an Error", function () {
     expect(function(){throw new J.Error();}).toThrowError();
   });
 
-  it("should get the correct stack in the details property", function(){
+  xit("should get the correct stack in the details property", function(){
     expect((new J.Error()).stack.slice(0, 50))
       .toEqual((new Error()).stack.slice(0, 50));
   });
